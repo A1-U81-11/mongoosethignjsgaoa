e6648f86c2c2816cbfb6fe86f5558513f0350f59
A1-U81-11 <kinggojira70@gmail.com>
2025-10-30 13:50:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ app.use(express.json())
 ///////////////// 
 mongoose.connect("mongodb+srv://martynasbarauskas_db_user:watermelon@cluster0.mlfo08f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>console.log("connected to DB")).catch((error) => console.log(error));
 
-const {createCourse, getAllRecords} =require ("./courseController");
+const {createCourse, getAllRecords, findOneCourse, updateCourseById, deleteOneCourse} =require ("./courseController");
 
 app.post("/courses", createCourse);
-app.get("/courses", getAllRecords);
+app.get("/allcourses", getAllRecords);
+app.get("/courses/:id", findOneCourse);
+app.put("/courses/:id", updateCourseById);
+app.delete("/courses/:id", deleteOneCourse);
 
 app.listen(5050, () => console.log("server is running on 5050 port"));
+
+
