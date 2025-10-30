197228172c07a4daf97e1a2f77d05c48a73c8249
A1-U81-11 <kinggojira70@gmail.com>
2025-10-30 13:49:37 +0200

    Update courseController.js

--- courseController.js
+++ courseController.js
@@ -30,7 +30,7 @@ const getAllRecords=async(req, res)=>{
 const myCoursesFromDB= await Course.find();
 
 if(myCoursesFromDB.length === 0){
-    returnres.status(404).json({ message: "No courses found in DB"});
+    return res.status(404).json({ message: "No courses found in DB"});
 }
 
 res.status(200).json(myCoursesFromDB)
@@ -40,6 +40,72 @@ res.status(200).json(myCoursesFromDB)
     }
 };
 
+// READ by ID
+// @ GET
+const findOneCourse=async(req,res)=> {
+    try{
+        const {id} = req.params;
+        const oneCourse = await Course.findById(id);
 
+        if(!oneCourse) {
+            return res.status(404).json({ message: "course no exist"})
 
-module.exports = { createCourse, getAllRecords };
+        }
+
+        res.status(200).json(oneCourse);
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({error: "internal server error, something wrong"})
+    }
+};
+
+// UPDATE
+// @POST
+
+const updateCourseById=async(req,res) => {
+    try{
+        const {id} = req.params;
+        const {title, teacher} = req.body;
+
+        if(!title || !teacher){
+            return res.status(400).json({error: "Data aint found" })
+        }
+
+        const courseFromDB = await Course.findById(id);
+
+        if (!courseFromDB) {
+            return res.status(404).json({message: "Course not found"})
+        }
+            courseFromDB.title = title;
+            courseFromDB.teacher = teacher;
+
+        const result = await courseFromDB.save();
+
+        res.status(200).json({ message: "record updated succesfully", result});
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error: "internal server error. Something wrong"});
+    }
+};
+
+// DELETE
+// @DELETE
+
+const deleteOneCourse = async (req, res)=>{
+    try{
+        const {id} = req.params;
+
+        const result = await Course.findByIdAndDelete(id);
+        if(!result){
+            return res.status(404).json({message: "Course not found"});
+        }
+        res.status(200).json({message: "course deleted yippee", result});
+
+    }     catch(err){
+        console.log(err);
+        res.status(500).json({error: "internal server error. Something wrong"});
+    }
+}
+
+module.exports = { createCourse, getAllRecords, findOneCourse, updateCourseById, deleteOneCourse};
